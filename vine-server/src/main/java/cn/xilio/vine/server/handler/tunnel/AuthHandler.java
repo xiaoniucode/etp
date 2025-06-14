f7f1c8529b2fcc3113c66ddad40bf0918f4632e8
xiaoniucode <yuanyicode@gmail.com>
2025-06-14 13:03:38 +0800

    客户端认证处理器、代理规则配置信息完善、通道管理器增加公网端口映射

--- vine-server/src/main/java/cn/xilio/vine/server/handler/tunnel/AuthHandler.java
+++ vine-server/src/main/java/cn/xilio/vine/server/handler/tunnel/AuthHandler.java
@@ -3,16 +3,39 @@ package cn.xilio.vine.server.handler.tunnel;
 import cn.xilio.vine.core.protocol.TunnelMessage;
 import cn.xilio.vine.server.ChannelManager;
 import cn.xilio.vine.core.AbstractMessageHandler;
-import com.google.protobuf.ByteString;
+import cn.xilio.vine.server.store.ProxyManager;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
 
 /**
  * 认证消息处理器
  */
 public class AuthHandler extends AbstractMessageHandler {
+    private final Logger logger = LoggerFactory.getLogger(AuthHandler.class);
+
     @Override
     protected void doHandle(ChannelHandlerContext ctx, TunnelMessage.Message msg) {
-        String secretKey = msg.getExt();
-        ChannelManager.addTunnelChannel(3307, secretKey, ctx.channel());
+        String secretKey = msg.getExt();//获取客户端传过来的认证密钥
+        //如果系统中不存在客户端的密钥，说明该客户端没有注册或者密钥错误
+        if (!ProxyManager.getInstance().isClientExist(secretKey)) {
+            logger.info("认证失败，客户端不存在！");
+            ctx.channel().close();//关闭当前通道
+            return;//返回方法，不执行后续的逻辑
+        }
+        //获取与客户端绑定的安全隧道channel
+        Channel tunnelChannel = ChannelManager.getTunnelChannel(secretKey);
+        //如果已经存在安全通道，表示已经认证过了，不能够重复认证，一个客户端只绑定一条安全通道-隧道
+        if (!ObjectUtils.isEmpty(tunnelChannel)) {
+            ctx.channel().close();//关闭当前客户端的通道
+        }
+        //获取客户端的所有内网服务对应的公网端口
+        List<Integer> internalPorts = ProxyManager.getInstance().getClientPublicNetworkPorts(secretKey);
+        //将客户端所有内网服务对应的公网端口绑定到已经成功认证的安全隧道通道上，后面所有数据会通过该通道传输
+        ChannelManager.addTunnelChannel(internalPorts, secretKey, ctx.channel());
     }
 }
