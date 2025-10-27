7ef0fd42bd14cb40909cc568c0ee8851787ef73c
xiaoniucode <yuanyicode@gmail.com>
2025-10-27 21:06:29 +0800

    feat: 完善代码日志

--- etp-client/src/main/java/cn/xilio/etp/client/TunnelClient.java
+++ etp-client/src/main/java/cn/xilio/etp/client/TunnelClient.java
@@ -14,12 +14,18 @@ import io.netty.channel.*;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLEngine;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * @author liuxin
+ */
 public class TunnelClient implements Lifecycle {
+    private final static Logger logger = LoggerFactory.getLogger(TunnelClient.class);
     private String serverAddr;
     private int serverPort;
     private String secretKey;
@@ -35,7 +41,7 @@ public class TunnelClient implements Lifecycle {
     /**
      * 最大延迟时间 如果超过了则取maxDelaySec为最大延迟时间 单位：秒
      */
-    private long maxDelaySec = 5;
+    private long maxDelaySec = 8;
     /**
      * 用于记录当前重试次数
      */
@@ -48,7 +54,9 @@ public class TunnelClient implements Lifecycle {
      * 隧道工作线程组
      */
     private EventLoopGroup tunnelWorkerGroup;
-
+    /**
+     * SSL加密上下文
+     */
     private SslContext sslContext;
 
     @Override
@@ -85,7 +93,8 @@ public class TunnelClient implements Lifecycle {
                                     .addLast(new TunnelMessageEncoder())
                                     .addLast(new IdleCheckHandler(60, 40, 0, TimeUnit.SECONDS))
                                     .addLast(new TunnelChannelHandler(realBootstrap, tunnelBootstrap, ctx -> {
-                                        retryCount.set(0); // 重置重试计数器
+                                        // 重置重试计数器
+                                        retryCount.set(0);
                                         //服务器断开 执行重试 重新连接
                                         scheduleReconnect();
                                     }));
@@ -94,7 +103,7 @@ public class TunnelClient implements Lifecycle {
             //连接到服务器
             connectTunnelServer();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage(), e);
         }
     }
 
@@ -109,8 +118,8 @@ public class TunnelClient implements Lifecycle {
                         .setExt(secretKey)
                         .build();
                 future.channel().writeAndFlush(message);
-                retryCount.set(0); // 重置重试计数器
-                AnsiLog.info("etp客户端启动成功");
+                retryCount.set(0);
+                AnsiLog.info("已连接到服务端");
             } else {
                 //重新连接
                 scheduleReconnect();
@@ -120,19 +129,18 @@ public class TunnelClient implements Lifecycle {
 
     private void scheduleReconnect() {
         if (retryCount.get() >= maxRetries) {
-            System.err.println("达到最大重试次数，停止重连");
+            AnsiLog.error("达到最大重试次数，停止重连");
             tunnelWorkerGroup.shutdownGracefully();
             return;
         }
-        // 计算退避时间 (2^n秒，最大不超过maxDelaySec)
+        // 计算退避时间 (最大不超过maxDelaySec)
         int retries = retryCount.getAndIncrement();
-        //指数计算，如果超过了最大延迟时间，则取最大延迟时间
-        // long delay = Math.min((1L << retries), maxDelaySec);
-        long delay = calculateDelay();//指数退避 + 随机抖动(±30%)
-        System.out.printf("[连接失败] 第%d次重连将在%d秒后执行%n", retries + 1, delay);
+        //指数退避
+        long delay = calculateDelay();
+        AnsiLog.error("连接失败],第{}次重连将在{}秒后执行", retries + 1, delay);
         // 调度重连任务
         tunnelWorkerGroup.schedule(() -> {
-            System.out.println("重连中...");
+            AnsiLog.info("重连中...");
             connectTunnelServer();
         }, delay, TimeUnit.SECONDS);
     }
