237d1ca834f406f09578653254a059f730f8dd5f
xiaoniucode <yuanyicode@gmail.com>
2025-11-01 22:49:50 +0800

    feat: update code and document.

--- etp-server/src/main/java/cn/xilio/etp/server/store/dto/ProxyDTO.java
+++
@@ -1,6 +0,0 @@
-package cn.xilio.etp.server.store.dto;
-
-import java.io.Serializable;
-
-public record ProxyDTO(String name, String protocol, Integer localPort, Integer remotePort) implements Serializable {
-}
